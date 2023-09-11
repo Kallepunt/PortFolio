71f5a86a88aac5883201de60b54ad67f7d4509fa
Okar <Okar@DESKTOP-4JU18SH>
2023-09-11 20:19:08 +0200

    What the dev

--- Pages/WhackMe/Game/GameInfo.cs
+++ Pages/WhackMe/Game/GameInfo.cs
@@ -1,11 +1,26 @@
-﻿namespace Portfolio.Pages.WhackMe.Game
+﻿using Portfolio.Pages.WhackMe.Game.Models;
+
+namespace Portfolio.Pages.WhackMe.Game
 {
 	public class GameInfo
 	{
 		public int Score = 0;
-		public int CurrentTime = 10;
-		public int HitPosition = 10;
+		public int CurrentTime = 20;
+		public int HitPosition;
 		public string Message = string.Empty;
-		public int GameSpeed = 1000;
-	}
+		public int GameSpeed = 600;
+		public bool IsGameRunning = true;
+
+
+		public List<SquareModel> Squares { get; set; } = new List<SquareModel>();
+
+
+        public GameInfo()
+        {
+				for (int i = 0; i < 9 ; i++)
+			{
+				Squares.Add(new SquareModel { Id = i });
+			}
+        }
+    }
 }
---
+++ Pages/WhackMe/Game/Models/SquareModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Portfolio.Pages.WhackMe.Game.Models
+{
+	public class SquareModel
+	{
+
+		public int Id { get; set; }
+		public string Style { get; set; }
+
+
+		private bool isShown;
+
+		public bool IsShown
+		{
+			get => isShown;
+			set
+			{
+				isShown = value;
+				if (isShown)
+				{
+					Style = "mole";
+				}
+				else
+				{
+					Style = string.Empty;
+				}
+			}
+		}
+
+	}
+}
