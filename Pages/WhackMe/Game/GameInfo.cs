1e5d96abc92ebaaa1ae97ae0aa977b0ae45c82d7
Okar <Okar@DESKTOP-4JU18SH>
2023-09-11 18:49:05 +0200

    .

---
+++ Pages/WhackMe/Game/GameInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Pages.WhackMe.Game
+{
+	public class GameInfo
+	{
+		public int Score = 0;
+		public int CurrentTime = 10;
+		public int HitPosition = 10;
+		public string Message = string.Empty;
+		public int GameSpeed = 1000;
+	}
+}
